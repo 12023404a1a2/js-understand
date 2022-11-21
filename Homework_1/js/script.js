4f3ded11fcedf4669a10fc18ff9e7d8ccd048b60
12023404a1a2 <118529401+12023404a1a2@users.noreply.github.com>
2022-11-21 14:15:46 +0500

    Add files via upload

---
+++ Homework_1/js/script.js
@@ -0,0 +1,6 @@
+let name = "Abbos"
+let surname = "Jo'rayev"
+let age = 14
+let frontend = "FE16"
+let  education = "Open Web Academy"
+console.log("Mening ismim " + name + " " + surname + ". Men " + age + " yoshdaman. " + "Men " + education + " fronend" + "da o'qiyman")
